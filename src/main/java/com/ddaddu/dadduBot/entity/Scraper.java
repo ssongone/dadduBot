d18b87bca53bcf345b0a4e73d8f22cdb5192db49
ssongone <41146676+ssongone@users.noreply.github.com>
2024-01-06 21:52:11 +0900

    Feat: 랭킹페이지에서 한번에 20개 가져오기
    랭킹 페이지 링크 입력하면 List<BookInfo> 반환

--- src/main/java/com/ddaddu/dadduBot/entity/Scraper.java
+++ src/main/java/com/ddaddu/dadduBot/entity/Scraper.java
@@ -7,9 +7,26 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+
 public class Scraper {
+
+    public List<BookInfo> scrapeBookList(String url) {
+        List<BookInfo> bookInfos = new ArrayList<>();
+        Document document = connect(url).orElseThrow(() -> new IllegalArgumentException("url이 잘못된 것 같습니다"));
+
+        Elements elements = document.select("div#contents div.detail p.title a");
+        for (Element element : elements) {
+            String hrefValue = element.attr("href");
+            bookInfos.add(scrapDetailPage(hrefValue));
+        }
+
+        System.out.println(bookInfos);
+        return bookInfos;
+    }
 
     public BookInfo scrapDetailPage(String url) {
         Document document = connect(url).orElseThrow(() -> new IllegalArgumentException("url이 잘못된 것 같습니다"));
@@ -19,8 +36,8 @@ public class Scraper {
         String isbn = document.select("div.mainItemTable tr:contains(ISBN)").first().select("span.codeSelect").text();
         String price = document.select("div.mainItemTable tr:contains(税込価格) td").first().text().replace(",", "").replace("円", "");
 
-        Element mainImageUrlElement = document.select("div#cover img").first();
-        String mainImageUrl = mainImageUrlElement.attr("src");
+        Element mainImageUrlElement = document.select("div.mainItemImg img").first();
+        String mainImageUrl = (mainImageUrlElement != null) ? mainImageUrlElement.attr("src") : "";
 
         String description = makeDescription(document, mainImageUrlElement);
 
@@ -35,10 +52,16 @@ public class Scraper {
     }
 
     private String makeDescription(Document document, Element mainImageUrlElement) {
+        if (mainImageUrlElement == null)
+            return "";
+
         String description = mainImageUrlElement.toString();
         Elements itemDetailContents = document.select("div.itemDetailContents");
         Elements commentContents = document.select("div.commentContents");
         Elements authorContents = document.select("div.authorContents");
+
+//        별점이 안가져와져서 보류..!
+//        Elements reviewContents = document.select("div.reviewContents");
 
         if (!itemDetailContents.isEmpty())
             description += itemDetailContents;
@@ -48,6 +71,9 @@ public class Scraper {
 
         if (!authorContents.isEmpty())
             description += authorContents;
+
+//        if (!reviewContents.isEmpty())
+//            description += reviewContents;
 
         return description;
     }
--- src/test/java/com/ddaddu/dadduBot/entity/ScraperTest.java
+++ src/test/java/com/ddaddu/dadduBot/entity/ScraperTest.java
@@ -2,6 +2,9 @@ package com.ddaddu.dadduBot.entity;
 
 
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class ScraperTest {
@@ -14,6 +17,20 @@ public class ScraperTest {
         assertThat(bookInfo.getTitle()).isEqualTo("大ピンチずかん　２");
         assertThat(bookInfo.getPrice()).isEqualTo(1650);
         assertThat(bookInfo.getMainImageUrl()).isEqualTo("https://www1.e-hon.ne.jp/images/syoseki/ac/98/34541798.jpg?impolicy=PC_AC_M");
+    }
+
+    @Test
+    void 랭킹정보한번에가져오기() {
+        Scraper scraper = new Scraper();
+        List<BookInfo> bookInfos = scraper.scrapeBookList("https://www.e-hon.ne.jp/bec/SE/Genre?dcode=06&ccode=01&Genre_id=060100");
+        assertThat(bookInfos.size()).isEqualTo(20);
+    }
+
+    @Test
+    void 이것저것해보는() {
+        Scraper scraper = new Scraper();
+        BookInfo bookInfo = scraper.scrapDetailPage("https://www.e-hon.ne.jp/bec/SA/Detail?refShinCode=0100000000000034483958&Action_id=121&Sza_id=B0");
+        System.out.println("bookInfo.getDescription() = " + bookInfo.getDescription());
 
     }
 }

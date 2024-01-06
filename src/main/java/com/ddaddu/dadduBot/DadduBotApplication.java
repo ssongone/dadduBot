8a87bb16a65650422cc41e0b0d90922314faa0bc
ssongone <41146676+ssongone@users.noreply.github.com>
2024-01-06 09:40:46 +0900

    Init project with basic structure and dependencies

---
+++ src/main/java/com/ddaddu/dadduBot/DadduBotApplication.java
@@ -0,0 +1,13 @@
+package com.ddaddu.dadduBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadduBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadduBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddaddu/dadduBot/DadduBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddaddu.dadduBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadduBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
